31af5cc4e24bd5f8e7331be112a9518633e51c16
Jeremy Greer <jeremy.greer.atl@gmail.com>
2015-06-01 21:55:35 -0400

    make system to setup/teardown playground for testing

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # filler v0.0.1 [![Build Status](https://travis-ci.org/reergymerej/filler.svg?branch=master)](https://travis-ci.org/reergymerej/rednib)
 
-
 It sucks making boilerplate stuff.  The lazy way is to have Node do it for you.
--- test/app.js
+++ test/app.js
@@ -1,9 +1,46 @@
 'use strict';
+
 var will = require('willy').will,
   app = require('../app');
+
+var path = require('path'),
+  fs = require('fs'),
+  files = [];
 
 describe('version', function () {
   it('should be 0.0.1', function () {
     will(app.version).be('0.0.1');
+  });
+});
+
+describe('main', function () {
+  var dirPath;
+
+  before(function () {
+    // setup an environment in which the app will run
+    dirPath = path.join(__dirname, 'playground');
+    fs.mkdirSync(dirPath);
+    files.push({ path: dirPath, dir: true });
+
+    fs.writeFileSync(path.join(dirPath, '.fillerrc'), JSON.stringify({
+      hello: 'dude'
+    }));
+
+    files.push({ path: path.join(dirPath, '.fillerrc'), dir: false });
+  });
+
+  after(function () {
+    // remove all the stuff we made for testing
+    files.reverse().forEach(function (file) {
+      if (file.dir) {
+        fs.rmdirSync(file.path);
+      } else {
+        fs.unlinkSync(file.path);
+      }
+    });
+  });
+
+  it('should read the config file for options', function () {
+
   });
 });
